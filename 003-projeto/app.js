21d77347af83bea9416db4e2b7de9162774e6c25
fabio1844 <110498510+fabio1844@users.noreply.github.com>
2022-08-12 21:04:10 -0300

    Add files via upload

--- 003-projeto/app.js
+++ 003-projeto/app.js
@@ -1,8 +1,21 @@
-var salario = 2000.00;
+var salario = 2000.50;
 var aumento = 0.15;
 var inflacao = 0.07;
 var novosalario = salario + ( aumento * salario);
 var novosalarioinflacao = novosalario - ( inflacao * novosalario);
 console.log ( novosalario); 
 console.log (novosalarioinflacao);
- 
+var strnovosalario = `o novo salario é :\n  R$\t ${novosalario}`;
+ var strnovosalarioinflacao = `o novo salario aplicada inflacao é :\n R$ ${novosalario}`
+console.log ( strnovosalario );
+console.log ( strnovosalarioinflacao );var salario = 2000.00;
+var aumento = 0.15;
+var inflacao = 0.07;
+var novosalario = salario + ( aumento * salario);
+var novosalarioinflacao = novosalario - ( inflacao * novosalario);
+console.log ( novosalario); 
+console.log (novosalarioinflacao);
+var strnovosalario = `o novo salario é :\n  R$\t ${novosalario}`;
+ var strnovosalarioinflacao = `o novo salario aplicada inflacao é :\n R$ ${novosalario}`
+console.log ( strnovosalario );
+console.log ( strnovosalarioinflacao );
