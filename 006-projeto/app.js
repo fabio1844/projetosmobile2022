1ba143350680c9d0db05366c4525fdf31329c1fe
fabio1844 <110498510+fabio1844@users.noreply.github.com>
2022-08-16 20:43:33 -0300

    Add files via upload

---
+++ 006-projeto/app.js
@@ -0,0 +1,10 @@
+var num1 = 10;
+var num2 = 15;
+var num3 = 5;
+console.log(num2 > num1);
+console.log(num1 > num3);
+if (num2 > num1 && num1 > num3)
+{console.log(`o numero ${num2} é maior que o ${num1}`) 
+} else {
+    console.log (`o numero ${num1} é menor que o numero ${num2}`)
+}
